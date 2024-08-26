8c985ed6a2e305487505941827a02d7ffb44020a
sandeeppal81 <65900914+sandeeppal81@users.noreply.github.com>
2024-08-26 20:29:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const scrape_session = require('./index/index').scrape_session;
+const { scrape_session } = require('./index/index');
+const path = require('path');
 
 app.get("/", async function (req, res) {
-  res.send("Hello World!");
-
   try {
     await scrape_session();
+    res.send('Screenshot taken and saved successfully.');
   } catch (error) {
-    console.log('Error in scrape_session:', error);
+    console.error('Error in scrape_session:', error);
+    res.status(500).send('An error occurred while taking the screenshot.');
   }
+});
 
+app.get('/screenshot', async function (req, res) {
+  try {
+    res.sendFile('/usr/src/app/screenshot.png');
+  } catch (error) {
+    console.error("Error sending the file:", error);
+    res.status(500).send("An error occurred while sending the file.");
+  }
 });
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
-
-
-async function tryCatchCallback(callback, ...args) {
-  try {
-      await callback(...args)
-  } catch (err){
-  }
-}
-
